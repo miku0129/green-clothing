49eb2b7531204cbd55f06ed48b3d0ce23298dc79
miku0129 <svartkatt39@gmail.com>
2023-06-24 14:12:36 +0200

    separate component Category-item

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
-import "./categories.styles.scss"
+import "./Categories.styles.scss"
+import CategoryItem from "./compoments/Category-item.component.jsx"
 
 const App = () => {
   const categories = [
-    {
-      id: 1,
-      title: "Hats",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-    },
-    {
-      id: 4,
-      title: "Womens",
-    },
-    {
-      id: 5,
-      title: "Mens",
-    },
-
-  ];
+  {
+    "id": 1,
+    "title": "hats",
+    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+  },
+  {
+    "id": 2,
+    "title": "jackets",
+    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+  },
+  {
+    "id": 3,
+    "title": "sneakers",
+    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+  },
+  {
+    "id": 4,
+    "title": "womens",
+    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+  },
+  {
+    "id": 5,
+    "title": "mens",
+    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+  }
+]
 
   return (
     <div className="categories-container">
       {categories.map((category) => {
         return (
-          <div className="category-container" key={category.id}>
-            <div className="background-image"></div>
-            <div className="category-body-container">
-              <h2>{category.title}</h2>
-              <p>Shop now</p>
-            </div>
-          </div>
+            <CategoryItem category={category} key={category.id}/>
         );
       })}
     </div>
---
+++ src/compoments/Category-item.component.jsx
@@ -0,0 +1,19 @@
+import "./Category-item.styles.scss";
+
+const CategoryItem = ({ category }) => {
+  const { title, imageUrl } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
