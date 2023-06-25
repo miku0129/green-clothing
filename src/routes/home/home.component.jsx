83b70cc4a1ae2c9b1da62e7b8a1f7ce3c1754dad
miku0129 <svartkatt39@gmail.com>
2023-06-25 15:59:28 +0200

    change shop images

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -6,27 +6,27 @@ const Home = () => {
     {
       id: 1,
       title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      imageUrl: "https://images.unsplash.com/photo-1576871337632-b9aef4c17ab9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
     },
     {
       id: 2,
       title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      imageUrl: "https://images.unsplash.com/photo-1604644401890-0bd678c83788?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
     },
     {
       id: 3,
       title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      imageUrl: "https://plus.unsplash.com/premium_photo-1667762240969-c1b16cf23a1c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=388&q=80",
     },
     {
       id: 4,
       title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      imageUrl: "https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
     },
     {
       id: 5,
       title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      imageUrl: "https://plus.unsplash.com/premium_photo-1681914039110-743b9f29a9fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMzfHwlRTclOTQlQjclRTYlODAlQTclMjAlRTclQUMlOTElRTklQTElOTQlMjAlRTglODclQUElRTclODQlQjZ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60",
     },
   ];
 
--- src/routes/nav/nav.component.jsx
+++ src/routes/nav/nav.component.jsx
@@ -1,14 +1,17 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
+import "./nav.styles.scss"
+
+import { ReactComponent as Shoplogo } from "../../assets/flower-svgrepo-com.svg";
 
 const Nav = () => {
   return (
     <Fragment>
-      <div className="nav">
+      <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>Logo</div>
+          <Shoplogo className="Shoplogo"/>
         </Link>
-        <div className="linkes-container">
+        <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
