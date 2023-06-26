a7ba056a36e258a81acad237ecc7e8452dbe0de0
miku0129 <svartkatt39@gmail.com>
2023-06-26 12:43:20 +0200

    update firebase-init.js

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
+
+import app from "../../firebase-init";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithRedirect,
+} from "firebase/auth";
+app();
+
 const SignIn = () => {
-    return (
+  const [hasLogin, setHasLogin] = useState(false);
+  console.log("haslogin ", hasLogin);
+  const signin = () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+
+    signInWithRedirect(auth, provider);
+    setHasLogin(true);
+  };
+  return (
+    <>
+      {hasLogin === false && (
         <div>
-            <h1>Sign In Page</h1>
+          <button onClick={signin}>login</button>
         </div>
-    )
-}
+      )}
+      {hasLogin === true && (
+        <div>
+          <h1>already login</h1>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default SignIn;
