d1a4b2f09d0f62a40d7ccc2a2372c7c77cc756fb
miku0129 <svartkatt39@gmail.com>
2023-07-08 13:52:03 +0200

    add style to sign-up-form.component

--- src/compoments/sign-up-form/sign-up-form.component.jsx
+++ src/compoments/sign-up-form/sign-up-form.component.jsx
@@ -4,6 +4,7 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
+import "./sign-up-form.styles.scss"; 
 
 const defaultFormField = {
   displayName: "",
@@ -40,8 +41,9 @@ const SignUpForm = () => {
     setField({ ...field, [name]: value }); //オブジェクト要素を更新
   };
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an accont?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="displayName"
