981e16359e2d6b6ef8f9c606060efaeb6aa61908
miku0129 <svartkatt39@gmail.com>
2023-07-24 14:32:35 +0200

    add netlify, stripe and payment-form

--- src/compoments/payment-form/payment-form.component.jsx
+++ src/compoments/payment-form/payment-form.component.jsx
@@ -1,14 +1,66 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
-import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+
+// import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
 
 const PaymentForm = () => {
-    return(
-        <div>
-            <CardElement />
-            <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now </Button>
-        </div>
-    )
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+    console.log("client_secret: ", client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "test user",
+        },
+      },
+    });
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Successful");
+      }
+    }
+  };
+
+  return (
+    // <PaymentFormContainer>
+    //   <FormContainer>
+    //     <h2>Credit Card Payment</h2>
+    //     <CardElement />
+    //     <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now </Button>
+    //   </FormContainer>
+    // </PaymentFormContainer>
+    <div>
+      <form onSubmit={paymentHandler}>
+        <h2>Credit Card Payment</h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now </Button>
+      </form>
+    </div>
+  );
 };
 
-export default PaymentForm; 
+export default PaymentForm;
