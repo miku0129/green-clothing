23ccc229658dbf5b5f2dedf806bffd722806eefe
miku0129 <svartkatt39@gmail.com>
2023-08-04 19:53:20 +0200

    updated category.component using with Redux

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,30 +1,32 @@
-import { useContext, useState, useEffect, Fragment } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
-
-// import { CategoriesContext } from "../../contexts/categories.context";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 
 import ProductCard from "../../compoments/product-card/product-card.component";
+
+import { selectCategories } from "../../store/categories/category.selector";
 
 import { CategoryTitle, CategoryContainer } from "./category.styles";
 
 const Category = () => {
-  // const { category } = useParams();
-  // const { categoriesMap } = useContext(CategoriesContext);
-  // const [products, setProducts] = useState([]);
+  const { category } = useParams();
+  const categoriesMap = useSelector(selectCategories);
 
-  // useEffect(() => {
-  //   setProducts(categoriesMap[category]);
-  // }, [category, categoriesMap]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
 
   return (
     <Fragment>
-      {/* <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+      <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
       <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </CategoryContainer> */}
+      </CategoryContainer>
     </Fragment>
   );
 };
